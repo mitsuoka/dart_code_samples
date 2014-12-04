14a803a92bb3b32df2e294bdda656069fd332291
Teruyoshi Mitsuoka <TerryMitsuoka@gmail.com>
2014-12-04 23:25:46 +0900

    zoned error added

--- codes/code_11.6b.dart
+++ codes/code_11.6b.dart
@@ -1,41 +1,42 @@
 import 'dart:async';
 main() {
-  try {
-    funcA();
+  var funcs = [funcA, funcB, funcC, funcD];
+  for (var func in funcs) {
+    try{
+    func(0);
+    }
+    on Error catch (e, stackTrace)  {print('Error! $e \nStack Trace :\n$stackTrace');
+    }
+    on Exception catch (e, stackTrace) {print('Exception! $e \nStack Trace :\n$stackTrace');
+    }
   }
-  on Error catch (er, st) {
-    print('Error : $er');
-    print(st);
-    }
-  on Exception catch (ex, st) {
-    print('Exception : $ex');
-    print(st);
-    }
-}
-
- funcA() {
-  funcB(0);
-//  funcC();
-//  funcD();
 }
 
 // ArgumentError
-funcB(x) {
+funcA(x) {
   if(x == 0) throw new ArgumentError('Generated ArgumentError');
 }
 
 // IntegerDivisionByZeroException
-funcC() {
+funcB(_) {
   int x = 1 % 0;
 }
 
 // AsyncError (Bad state: More than one element)
-funcD() {
+funcC(_) {
   final testData = ['a', 'b'];
   var stream = new Stream.fromIterable(testData);
   stream.single.then((value){})
-    .catchError((e, stackTrace){
-      print('AsyncError : ${e}');
-      print('Stack Trace : ${stackTrace}');
-      });
+    .catchError((e, stackTrace) => print('AsyncError! $e \nStack Trace :\n$stackTrace'));
+}
+
+//AsyncError (Same error but caught in a zone)
+funcD(_) {
+  final testData = ['a', 'b'];
+  var stream = new Stream.fromIterable(testData);
+  runZoned(() {
+    stream.single.then((value){
+    });
+  },
+  onError: (e, stackTrace) => print('AsyncError(zoned)! $e \nStack Trace :\n$stackTrace'));
 }
