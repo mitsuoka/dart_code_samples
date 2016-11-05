a4dab42035d1fb0084a69a66fc8dc5a8f61bf660
Teruyoshi Mitsuoka <terrymitsuoka@gmail.com>
2016-11-05 15:30:10 +0900

    code_06.2.dart revised

--- codes/code_06.2.dart
+++ codes/code_06.2.dart
@@ -1,22 +1,25 @@
-class Greeter implements Comparable {
-  String prefix = 'Hello,';
-  Greeter() {}  // default constructor
-  Greeter.withPrefix(this.prefix); // named constructor
-  greet(String name) => print('$prefix $name'); // print greet
-  int compareTo(Greeter other) => prefix.compareTo(other.prefix); // compare prefixes
-}
-
-void main() {
-  Greeter greeter = new Greeter();
-  Greeter greeter2 = new Greeter.withPrefix('Hi,');
-
-  num result = greeter2.compareTo(greeter);
-  if (result == 0) {
-    greeter2.greet('you are the same.');
-  } else {
-    greeter2.greet('you are different.');
+abstract class Describable {
+  void describe();
+  void describeWithEmphasis() {
+    print('========');
+    describe();
+    print('========');
   }
 }
-/*
-Hi, you are different.
-*/
+
+class MyTitle extends Describable {
+  void describe() => print("General Manager");
+}
+class MySubtitle implements Describable {
+  void describe() => print("Development Division");
+  void describeWithEmphasis() {
+    print('********');
+    describe();
+    print('********');
+  }
+}
+
+main(){
+  new MyTitle().describeWithEmphasis();
+  new MySubtitle().describeWithEmphasis();
+}
